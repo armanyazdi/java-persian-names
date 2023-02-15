91dd99f0e4d173932dcf94a591ca09b84d314f43
Arman Yazdi <f41c0n.32@gmail.com>
2023-02-15 05:37:01 +0330

    Update Main.java

--- src/main/java/armanyazdi/Main.java
+++ src/main/java/armanyazdi/Main.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
         PersianNames name = new PersianNames();
 
         // Generate Persian names in Farsi:
