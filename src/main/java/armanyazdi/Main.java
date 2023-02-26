273b14f6b46359e96d8998c5b2ef629426421332
Arman Yazdi <f41c0n.32@gmail.com>
2023-02-26 05:12:46 +0330

    Update Main.java

--- src/main/java/armanyazdi/Main.java
+++ src/main/java/armanyazdi/Main.java
@@ -4,16 +4,16 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        // Generate Persian names in Farsi:
-        System.out.println(PersianNames.fullNameFa("male"));
-        System.out.println(PersianNames.fullNameFa("female"));
-        System.out.println(PersianNames.fullNameFa("random"));
-        System.out.println(PersianNames.fullNameFa()); // Default: "random"
+        // Generate Persian Full Names in Farsi:
+        System.out.println(PersianNames.fullNameFarsi("male"));
+        System.out.println(PersianNames.fullNameFarsi("female"));
+        System.out.println(PersianNames.fullNameFarsi("random"));
+        System.out.println(PersianNames.fullNameFarsi()); // Default: "random"
 
-        // Generate Persian names in English:
-        System.out.println(PersianNames.fullNameEn("m"));
-        System.out.println(PersianNames.fullNameEn("f"));
-        System.out.println(PersianNames.fullNameEn("r"));
-        System.out.println(PersianNames.fullNameEn()); // Default: "random"
+        // Generate Persian Full Names in English:
+        System.out.println(PersianNames.fullNameEnglish("m"));
+        System.out.println(PersianNames.fullNameEnglish("f"));
+        System.out.println(PersianNames.fullNameEnglish("r"));
+        System.out.println(PersianNames.fullNameEnglish()); // Default: "random"
     }
 }
